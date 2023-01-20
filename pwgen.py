fdee8b7a2827c45bafec60df27205b4d2059d614
intoam <122504581+intoam@users.noreply.github.com>
2023-01-20 02:54:14 +0000

    Add files via upload

---
+++ pwgen.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+# -----------------------------------------------------------------------------------------------
+# a password generator
+#
+# this tool will generate 12 string passwords containing; letters, numbers and special characters
+# encryption for the win!
+# -----------------------------------------------------------------------------------------------
+
+print("new password generated!")
+
+import secrets
+import string
+
+
+def create_pw(pw_length=12):
+   letters = string.ascii_letters
+   digits = string.digits
+   special_chars = string.punctuation
+
+   alphabet = letters + digits + special_chars
+   pwd = ''
+   pw_strong = False
+
+   while not pw_strong:
+       pwd = ''
+       for i in range(pw_length):
+           pwd += ''.join(secrets.choice(alphabet))
+
+       if (any(char in special_chars for char in pwd) and
+               sum(char in digits for char in pwd) >= 2):
+           pw_strong = True
+
+   return pwd
+
+
+if __name__ == '__main__':
+   print(create_pw())
